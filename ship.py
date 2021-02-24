9602126d5b82d9aaba2c307ac2f72db139518ccd
ItPingGe <ItPingGe@outlook.com>
2021-02-24 21:41:26 +0800

    创建ship类

--- ship.py
+++ ship.py
@@ -1,2 +1,23 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import pygame
+
+class Ship():
+
+    def __init__(self, screen):
+        """初始化飞船并设置其初始位置"""
+        self.screen = screen
+
+        # 加载飞船图像并获取其外接矩形
+        self.image = pygame.image.load('/Users/yanweiping/Documents/python/Aline/images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # 将每艘新飞船放在屏幕底部中央
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitem(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image, self.rect)
