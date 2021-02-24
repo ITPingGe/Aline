8bb63b6576f7002a03cb9958db85999e08155a1f
ITPingGe <ITPingGe@outlook.com>
2021-02-25 01:15:13 +0800

    创建函数update_screen()

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,10 +22,7 @@ def run_game():
         # 监视键盘和鼠标事件
         gf.check_events()
         # 每次循环时都重新绘制屏幕
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
         # 让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
