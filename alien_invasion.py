d262c0939c86a240ab2a08279d1bdc875cd15f81
ITPingGe <ITPingGe@outlook.com>
2021-02-24 23:48:33 +0800

    重构：模块game_functions

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -13,15 +14,16 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    # 创建一艘飞船
+    ship = Ship(screen)
+
     # 开始游戏的主循环
     while True:
-
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
+        gf.check_events()
+        # 每次循环时都重新绘制屏幕
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
--- ship.py
+++ ship.py
@@ -10,7 +10,8 @@ class Ship():
         self.screen = screen
 
         # 加载飞船图像并获取其外接矩形
-        self.image = pygame.image.load('/Users/yanweiping/Documents/python/Aline/images/ship.bmp')
+        # self.image = pygame.image.load('/Users/yanweiping/Documents/python/Aline/images/ship.bmp')
+        self.image = pygame.image.load('F:\creation\Python\Aline\images\ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
@@ -18,6 +19,6 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-    def blitem(self):
+    def blitme(self):
         """在指定位置绘制飞船"""
         self.screen.blit(self.image, self.rect)
