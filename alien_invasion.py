759b48162de18aacc837afeed02d593c4e4a66df
ItPingGe <ItPingGe@outlook.com>
2021-02-25 18:25:37 +0800

    重构check_events()

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,12 +15,13 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # 创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         # 每次循环时都重新绘制屏幕
         # 让最近绘制的屏幕可见
         gf.update_screen(ai_settings, screen, ship)
