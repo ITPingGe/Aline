fb003e6b032427d21e8ce448b70cec3d1c3bf8a4
ITPingGe <ITPingGe@outlook.com>
2021-03-02 23:15:12 +0800

    创建第一个外星人

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from aline import Aline
 import game_functions as gf
 from pygame.sprite import Group
 
@@ -19,6 +20,8 @@ def run_game():
     ship = Ship(ai_settings, screen)
     # 创建一个用于存储子弹的编组
     bullets = Group()
+    # 创建一个外星人
+    aline = Aline(ai_settings, screen)
 
     # 开始游戏的主循环
     while True:
@@ -28,6 +31,6 @@ def run_game():
         gf.update_bullets(bullets)
         # 每次循环时都重新绘制屏幕
         # 让最近绘制的屏幕可见
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aline, bullets)
 
 run_game()
