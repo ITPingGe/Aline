47c1317cba0f0cab0f33e7c4cb473abb8a1bbb6c
ITPingGe <ITPingGe@outlook.com>
2021-03-09 00:11:48 +0800

    增加外星人击落逻辑

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,7 +28,7 @@ def run_game():
         # 监视键盘和鼠标事件
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_bullets(bullets)
+        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
         gf.update_aliens(ai_settings, aliens)
         # 每次循环时都重新绘制屏幕
         # 让最近绘制的屏幕可见
